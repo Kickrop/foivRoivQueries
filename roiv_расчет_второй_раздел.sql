d8e1cc9bb0bdcf7f60b697e080c1c302750cbc5e
martynov <kickrop@gmail.com>
2019-03-28 10:56:25 +0300

    1,2,3 ROIV razdel quries

---
+++ roiv_расчет_второй_раздел.sql
@@ -0,0 +1,54 @@
+﻿with data as (
+		select	str_num,
+			x1_yes, 
+			x1_no,
+			x1_total,
+			x1_pusto, 
+			round(x1_yes::numeric/x1_total::numeric*100, 2) x1_yes_cut, 
+			round(x1_no::numeric/x1_total::numeric*100,2) x1_no_cut,
+			round(x1_total::numeric/x1_total::numeric*100,2) x1_total_cut,
+			round(x1_pusto::numeric/x1_total::numeric*100,2) x1_pusto_cut
+-- 			,
+-- 			x2_yes, 
+-- 			x2_no,
+-- 			x2_total,
+-- 			x2_pusto, 
+-- 			round(x2_yes::numeric/x2_total::numeric*100, 2) x2_yes_cut, 
+-- 			round(x2_no::numeric/x2_total::numeric*100,2) x2_no_cut,
+-- 			round(x2_total::numeric/x2_total::numeric*100,2) x2_total_cut,
+-- 			round(x2_pusto::numeric/x2_total::numeric*100,2) x2_pusto_cut
+		from (
+			select
+				str_num,
+				count(case when x1 = '1' then x1 end) x1_yes,
+				count(case when x1::int = 2 then 2 end) x1_no, --or x1 is null
+				count(case when x1 is null then 100 end) x1_pusto,
+				count(case when x1::int = 2 or x1::int = 1 or x1 is null then 99 end) x1_total
+-- 				,
+-- 				count(case when x2 = '1' then x2 end) x2_yes,
+-- 				count(case when x2::int = 2 then 2 end) x2_no, --or x2 is null
+-- 				count(case when x2 is null then 100 end) x2_pusto,
+-- 				count(case when x2::int = 2 or x2::int = 1 or x2 is null then 99 end) x2_total
+			from (
+				select *
+				from
+				get_google_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vTqBEet15-vvXa7M-nJhh6jG8c2wnl3E5d6YnwJkdXO1tRIGln5K-l34M_j5qYKzwXPqkayQNe0Okxa/pub?gid=943972237&single=true&output=csv'::text) a(id text, str_name text, str_num text, x1 text)
+				--where str_num = '1' --in ('501', '502', '503', '514')
+				) d1		
+			group by str_num
+			) d2
+		)
+-- select *
+-- from data
+select *
+from (
+	select str_num, 'Да' as val, x1_yes x1, x1_yes_cut x1_cut, '1' answ_id		
+		from data
+	union all select str_num, 'Нет' as val,  x1_no, x1_no_cut, '2' answ_id		
+		from data
+	union all select str_num, 'Без ответа' as val,  x1_pusto, x1_pusto_cut, '3' answ_id
+		from data 
+	union all select str_num, 'Всего' as val, x1_total, x1_total_cut, '4' answ_id
+		from data 
+	) d3
+order by str_num::int, answ_id
